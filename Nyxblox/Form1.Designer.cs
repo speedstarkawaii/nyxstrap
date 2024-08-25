1c034386b865f67c089406357786338e24a91051
speedsterkawaii <avtarvirdi82@gmail.com>
2024-08-24 23:20:32 -0400

    Add files via upload

---
+++ Nyxblox/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Nyxblox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.guna2ShadowForm1 = new Guna.UI2.WinForms.Guna2ShadowForm(this.components);
+            this.guna2ProgressBar1 = new Guna.UI2.WinForms.Guna2ProgressBar();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // guna2ShadowForm1
+            // 
+            this.guna2ShadowForm1.BorderRadius = 70;
+            this.guna2ShadowForm1.TargetForm = this;
+            // 
+            // guna2ProgressBar1
+            // 
+            this.guna2ProgressBar1.Location = new System.Drawing.Point(27, 278);
+            this.guna2ProgressBar1.Name = "guna2ProgressBar1";
+            this.guna2ProgressBar1.ProgressColor = System.Drawing.Color.Pink;
+            this.guna2ProgressBar1.ProgressColor2 = System.Drawing.Color.Pink;
+            this.guna2ProgressBar1.Size = new System.Drawing.Size(498, 25);
+            this.guna2ProgressBar1.TabIndex = 1;
+            this.guna2ProgressBar1.Text = "guna2ProgressBar1";
+            this.guna2ProgressBar1.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SingleBitPerPixel;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox3.Image = global::Nyxblox.Properties.Resources.icons8_x_50__1_;
+            this.pictureBox3.Location = new System.Drawing.Point(496, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(37, 33);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Nyxblox.Properties.Resources._250841_after;
+            this.pictureBox2.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(73, 58);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.pictureBox1.Image = global::Nyxblox.Properties.Resources.rsz_1image__10__removebg_preview;
+            this.pictureBox1.Location = new System.Drawing.Point(116, 67);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(312, 205);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 7F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(17, 310);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(516, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "The Roblox logo is a trademark of Roblox Corporation and is the property of its r" +
+    "espective owners. All rights reserved.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(547, 329);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.guna2ProgressBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "   ";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private Guna.UI2.WinForms.Guna2ShadowForm guna2ShadowForm1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private Guna.UI2.WinForms.Guna2ProgressBar guna2ProgressBar1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Nyxblox/Form1.cs
@@ -0,0 +1,266 @@
+﻿using Guna.UI2.WinForms;
+using Guna.UI2.WinForms.Suite;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nyxblox
+{
+    public partial class Form1 : Form
+    {
+        //open source roblox launcher i made
+        //its limited not really a launcher just something in freetimeeee
+        //.gg/getnyx
+
+
+        //basic confug
+        private Timer timer;
+        private int targetValue = 100;
+        private int increment = 1; 
+        private int currentValue = 0; 
+
+        private bool dragging = false;
+        private Point dragCursorPoint;
+        private Point dragFormPoint;
+
+        //some imports we can use i can do it in C but c# is bettr
+        [DllImport("user32.dll")]
+        private static extern bool IsWindowVisible(IntPtr hWnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool SetWindowText(IntPtr hWnd, string lpString);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+        private static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+        private static readonly IntPtr HWND_NOTOPMOST = new IntPtr(-2);
+        private const uint SWP_NOSIZE = 0x0001;
+        private const uint SWP_NOMOVE = 0x0002;
+
+
+        const int GWL_STYLE = -16;
+        const int WS_MINIMIZEBOX = 0x20000;
+
+        public Form1()
+        {
+            InitializeComponent();
+            StartProgress();
+
+            //for some reason creating events in the properties window froze vs :C
+            this.MouseDown += new MouseEventHandler(Form1_MouseDown);
+            this.MouseMove += new MouseEventHandler(Form1_MouseMove);
+            this.MouseUp += new MouseEventHandler(Form1_MouseUp);
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            dragging = true;
+            dragCursorPoint = Cursor.Position;
+            dragFormPoint = this.Location;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (dragging)//moves location; theres other ways to do this but for my shadow to work i had to make it move :/
+            {
+                Point dif = Point.Subtract(Cursor.Position, new Size(dragCursorPoint));
+                this.Location = Point.Add(dragFormPoint, new Size(dif));
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            dragging = false;
+        }
+
+        private void StartProgress()//when we start the app we want to open roblox
+        {
+
+
+            timer = new Timer
+            {
+                Interval = 8//slow
+            };
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (currentValue < targetValue)
+            {
+
+                currentValue += increment;//dont use while true gunabarthingie + 1 its badd
+                guna2ProgressBar1.Value = Math.Min(currentValue, targetValue); 
+            }
+            else
+            {
+                timer.Stop();
+                LaunchRobloxPlayer();//must be launched after progressbar otherwise the code thinks that Roblox is already open
+                //ps: check the last line of code here u can see why
+
+            }
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)//close roblox if user presses x
+        {
+            Process[] processes = Process.GetProcessesByName("RobloxPlayerBeta");
+
+            if (processes.Length > 0)
+            {
+                foreach (Process process in processes)
+                {
+                    process.Kill();
+                    Environment.Exit(-1);//close current app as well ofc
+                 
+                }
+            }
+            else
+            {
+                Environment.Exit(-1);
+            }
+        }
+
+        private void LaunchRobloxPlayer()
+        {
+            string a = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            string rbxx = Path.Combine(a, "Roblox");
+
+            string f = SearchForRobloxPlayer(rbxx, "RobloxPlayerBeta.exe");//EXACT name; lots of ppl use weird directories so needs diff approach
+
+            if (!string.IsNullOrEmpty(f))
+            {
+                Process.Start(f);
+            }
+            else
+            {
+                MessageBox.Show("Roblox was NOT found. You may be using a custom directory for roblox.\n\nContact @speedsterkawaii to resolve this.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private string SearchForRobloxPlayer(string directory, string fileName)
+        {
+            try
+            {
+                string[] files = Directory.GetFiles(directory, fileName, SearchOption.AllDirectories);
+                return files.Length > 0 ? files[0] : null; 
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return null;
+            }
+            catch (Exception ex)//handle ex in textbabel later
+            {
+                return null;
+            }
+        }
+
+        private async void CheckForRobloxWindowAsync()//IMPORT func this will properly dispose the app
+        {
+            while (true)
+            {
+                var robloxWindow = Process.GetProcesses()//pov byfron
+                    .Where(p => p.MainWindowTitle.Contains("Roblox") && IsWindowVisible(p.MainWindowHandle))
+                    .FirstOrDefault();
+
+                if (robloxWindow != null)
+                    //THIS IS WHERE YOU DO THE STUFF AFTER YOUR APP CLOSES
+                {
+                    //RenameRobloxWindow();//bad i was gonna do it later but i realized i had to grab the window title in injector sooooo
+
+                    string rbx = "Roblox";
+
+                    // Find the window handle
+                    IntPtr hWnd = FindWindow(null, rbx);
+
+                    if (hWnd == IntPtr.Zero)
+                    {
+                        //Console.WriteLine("Window not found.");
+                        return;
+                    }
+
+                    SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
+
+                    Application.Exit();//CLOSE app the moment roblox is found
+                    return; 
+                }
+
+                await Task.Delay(1111);//slow delay but my cpu ded if i make it even faster
+            }
+        }
+
+        //private void RenameRobloxWindow()
+        //{
+        //    // Get the Roblox window handle
+        //    var robloxWindow = Process.GetProcesses()
+        //        .FirstOrDefault(p => p.MainWindowTitle.Contains("Roblox"));
+
+        //    if (robloxWindow != null)
+        //    {
+        //        // Set the title to "hi"
+        //        bool result = SetWindowText(robloxWindow.MainWindowHandle, "Roblox");
+
+        //        if (result)
+        //        {
+        //        }
+        //        else
+        //        {
+        //        }
+        //    }
+        //    else
+        //    {
+        //    }
+        //}
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Process[] processes = Process.GetProcessesByName("RobloxPlayerBeta");
+
+            if (processes.Length > 0)
+            {
+                foreach (Process process in processes)
+                {
+                    process.Kill();
+
+                }
+            }
+            else
+            {
+            }
+
+            //BELOW CODE IS TECHNICALLY USELESS LOL
+            var robloxProcesses = Process.GetProcessesByName("RobloxPlayerBeta");
+
+            if (robloxProcesses.Length > 0)
+            {//already opened roblox. just do a warning. we can unlock a mutex of roblox but thats todo in another project
+                MessageBox.Show("Another instance is already executed onto this computer.", "NYX", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Environment.Exit(-1);
+            }
+            else
+            {
+            }
+            CheckForRobloxWindowAsync();
+        }
+    }
+}
